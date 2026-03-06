5cc1576196191e4aef3ee2c574a90902fbf92f9d
minetoiles <byeol9253@gmail.com>
2026-03-06 22:18:49 +0900

    [BOJ] Solve Sprout problems (2026-03-06)

---
+++ BOJ/Sprout/1008_A_나누기_B.py
@@ -0,0 +1,4 @@
+# BOJ 1008 A/B
+# 공백처리 때문에 split() 사용
+a, b = map(float, input().split())
+print(a / b)
---
+++ BOJ/Sprout/10896_사칙연산.py
@@ -0,0 +1,8 @@
+# BOJ 10896 사칙연산
+# 나누기는 /, 몫은 //, 나머지는 %
+a, b = map(int, input().split())
+print(a + b)
+print(a - b)
+print(a * b)
+print(a // b)
+print(a % b)
