c51d25d27219cb7d7a82fae8d194b01fa55f65a6
minetoiles <byeol9253@gmail.com>
2026-03-27 15:56:45 +0900

    [BOJ] Solve Silver problem (2026-03-27)

--- BOJ/Silver/10828_스택.py
+++ BOJ/Silver/10828_스택.py
@@ -2,60 +2,35 @@
 
 import sys
 input = sys.stdin.readline
-
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-class Stack:
-    def __init__(self):
-        self.top = None
-        self.count = 0
-
-    def push(self, data):
-        node = Node(data)
-        node.next = self.top
-        self.top = node
-        self.count += 1
-
-    def pop(self):
-        if self.top is None:
-            return -1
-        node = self.top
-        self.top = self.top.next
-        self.count -= 1
-        return node.data
-
-    def peek(self):
-        if self.top is None:
-            return -1
-        return self.top.data
-
-    def is_empty(self):
-        return 1 if self.top is None else 0
-
-    def size(self):
-        return self.count
-
-
-s = Stack()
+from collections import deque
 n = int(input())
 
+s = deque()
 result = []
 
-for _ in range(n):
+for i in range (n):
     cmd = input().split()
-
-    if cmd[0] == "push":
-        s.push(int(cmd[1]))
-    elif cmd[0] == "pop":
-        result.append(str(s.pop()))
-    elif cmd[0] == "size":
-        result.append(str(s.size()))
-    elif cmd[0] == "empty":
-        result.append(str(s.is_empty()))
-    elif cmd[0] == "top":
-        result.append(str(s.peek()))
-
+    
+    if (cmd[0] == "push"):
+        s.append(int(cmd[1]))
+    elif (cmd[0] == "pop"):
+        if (len(s) == 0):
+            result.append("-1")
+        else:
+            result.append(str(s.pop()))
+    elif (cmd[0] == "size"):
+        result.append(str(len(s)))
+    elif (cmd[0] == "empty"):
+        if (len(s) == 0):
+            result.append("1")
+        else:
+            result.append("0")
+    elif (cmd[0] == "top"):
+        if (len(s) == 0):
+            result.append("-1")
+        else:
+            num = s.pop()
+            result.append(str(num))
+            s.append(num)
+            
 print("\n".join(result))
