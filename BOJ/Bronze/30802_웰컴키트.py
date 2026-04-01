3da6e156a5faa3b35be419d9ab9f9540e6a2b8ef
minetoiles <byeol9253@gmail.com>
2026-04-01 17:54:16 +0900

    add a comment

--- BOJ/Bronze/30802_웰컴키트.py
+++ BOJ/Bronze/30802_웰컴키트.py
@@ -1,4 +1,4 @@
-
+# BOJ 30802 웰컴키트
 n = int(input())
 tsize = list(map(int, input().split()))
 t, p = map(int, input().split())
@@ -6,13 +6,14 @@ t, p = map(int, input().split())
 tsum = 0
 for tshirt in tsize:
     leftover = tshirt % t
-    tshirt //= t
-    if (tshirt == 0):
+    tnumber = tshirt // t
+    if (tshirt != 0 and tnumber == 0):
         tsum += 1
-    elif (leftover != 0):
-        tsum += (tshirt + 1)
     else:
-        tsum += tshirt
+        if (leftover != 0):
+            tsum += (tnumber + 1)
+        else:
+            tsum += tnumber
 
 print(tsum)
 print(n//p, n%p)
